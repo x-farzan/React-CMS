e302ad47ab54695f01bb9681de1601e6e0234e63
x-farzan <farzanhassan245@gmail.com>
2024-08-28 01:30:12 +0500

    Added work section and work items

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import Main from "./components/Main";
 import Sidenav from "./components/Sidenav";
+import Work from "./components/Work";
 
 function App() {
   return (
     <>
       <Sidenav />
       <Main />
-      {/* <Work /> */}
+      <Work />
     </>
   );
 }
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import React from "react";
+import WorkItem from "./WorkItem";
 
+const data = [
+  {
+    id: "1",
+    year: "2020",
+    title: "Convergence Business Systems",
+    duration: "1 year",
+    details: "will write detail soon, stay tuned .... ",
+  },
+  {
+    id: "1",
+    year: "2021",
+    title: "OptimusFox",
+    duration: "1 year",
+    details: "will write detail soon, stay tuned .... ",
+  },
+  {
+    id: "1",
+    year: "2022",
+    title: "Developers Studio",
+    duration: "1 year",
+    details: "will write detail soon, stay tuned .... ",
+  },
+  {
+    id: "1",
+    year: "2023",
+    title: "TekHQS",
+    duration: "Presently working",
+    details: "will write detail soon, stay tuned .... ",
+  },
+];
 const Work = () => {
   return (
-    <div>
-      Work
+    <div id="work" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
+      <h1 className="text-4xl font-bold text-center text-[#081b53]">Work</h1>
+      {data.map((item, idx) => (
+        <WorkItem
+          key={idx}
+          year={item.year}
+          title={item.title}
+          duration={item.duration}
+          details={item.details}
+        />
+      ))}
     </div>
-  )
-}
-
-export default Work
+  );
+};
+export default Work;
