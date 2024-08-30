7c173a961e367d56feaeb7a56ab7ee54bad5c65a
x-farzan <farzanhassan245@gmail.com>
2024-08-31 01:30:37 +0500

    Implementeed contact form

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Main from "./components/Main";
 import Project from "./components/Project";
 import Sidenav from "./components/Sidenav";
 import Work from "./components/Work";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Main />
       <Work />
       <Project />
+      <Contact />
     </>
   );
 }
