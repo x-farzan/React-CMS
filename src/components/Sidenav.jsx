02bb3450674810d0fa1848b87b1a76125bfe4929
x-farzan <farzanhassan245@gmail.com>
2024-08-31 01:33:28 +0500

    Fixed navbar closing issue for mobile screen

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -23,6 +23,7 @@ const Sidenav = () => {
       {nav ? (
         <div className="fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20">
           <a
+            onClick={handleNav}
             href="#main"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
@@ -31,6 +32,7 @@ const Sidenav = () => {
           </a>
 
           <a
+            onClick={handleNav}
             href="#work"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
@@ -39,6 +41,7 @@ const Sidenav = () => {
           </a>
 
           <a
+            onClick={handleNav}
             href="#projects"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
@@ -47,6 +50,7 @@ const Sidenav = () => {
           </a>
 
           <a
+            onClick={handleNav}
             href="#main"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
@@ -55,6 +59,7 @@ const Sidenav = () => {
           </a>
 
           <a
+            onClick={handleNav}
             href="#contact"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
